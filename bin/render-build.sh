01c2bb50efcc13ee3dadd2cbbf41af1bfc1d45f9
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2023-01-13 07:47:39 +0300

    changer render build file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,7 @@ set -o errexit
 
 # Build commands for back end
 bundle install
-bundle exec rake db:migrate
+rails db:reset
+ 
 bundle exec rake db:migrate
 bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only to avoid duplicate records
